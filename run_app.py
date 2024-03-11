833cc3817ba9546a6561dd7358264cc003aa6d9d
Yamila Timmer <y.timmer.2@st.hanze.nl>
2024-03-11 18:48:25 +0100

    Made a form for input elements in index.html, made it so that the input gets saved with flask.

--- run_app.py
+++ run_app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/', methods=['POST', 'GET'])
 def webtool():
-    return render_template("index.html", webtool_active=True)
+    if request.method == 'GET':
+        # default response when a form is called, renders index.html
+        return render_template("index.html", webtool_active=True)
+
+    elif request.method == 'POST':
+        # response when the submit button is clicked in index.html
+        # packs the variables in dictionary 'kwargs'
+        kwargs = {
+            'pdb_file': request.form['pdb_file'],
+            'mol2_file': request.form['mol2_file'],
+            'dock_slider': request.form['dock_slider'],
+            'RMSD_slider': request.form['RMSD_slider'],
+            'name_file': request.form['name_file'],
+        }
+        # render the 'form_POST.html' with the variables collected from the form in index.html
+        return render_template('form_POST.html', **kwargs)
 
 @app.route("/ourteam")
 def our_team():
