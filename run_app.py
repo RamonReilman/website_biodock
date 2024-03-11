e282a5b28407e4cb91ec722072154292cc253016
svermeulen <vermeulen.stijn06@gmail.com>
2024-03-11 17:02:38 +0100

    added base.html and changed the corresonding files to make it work
    the active command is changed to a True/False statement because giving nothin in run_app.py returns False.

--- run_app.py
+++ run_app.py
@@ -3,19 +3,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def webtool():
-    return render_template("index.html")
+    return render_template("index.html", webtool_active=True)
 
 @app.route("/ourteam")
 def our_team():
-    return render_template("our_team.html")
+    return render_template("our_team.html", ourteam_active=True)
 
 @app.route("/history")
 def history():
-    return render_template("history.html")
+    return render_template("history.html", history_active=True)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", about_active=True)
 
 if __name__ == "__main__":
     app.debug = True
