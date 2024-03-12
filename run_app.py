c944bab9b5dae8e81b77f0e0506f7a3932809b20
Ramon Reilman <r.reilman#st.hanze.nl>
2024-03-12 11:45:52 +0100

    Finished the history page
    It now works dynamically, and can delete the history (don't yet)

--- run_app.py
+++ run_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 import os
+from used_functions.functions_hist_page import clear_me
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,15 +13,25 @@ def our_team():
 
 @app.route("/history", methods=["POST", "GET"])
 def history():
+    path = "templates/history"
+    dir_list = os.listdir(path)
+
     if request.method == "GET":
-        path = "templates/history"
-        dir_list = os.listdir(path)
         return render_template("history.html", files=dir_list, history_active=True)
-    
+
     elif request.method == "POST":
         file_wanted = list(request.form.keys())
         file_wanted = str(file_wanted).replace("[", "").replace("]", "").replace("'", "")
-        return render_template(f"history/{file_wanted}/temp.html")
+
+        if file_wanted == "clear_me":
+            print("Everything has been deleted")
+
+            # uncomment to enable deleting
+            # clear_me()
+            return redirect("/")
+
+        else:
+            return render_template(f"history/{file_wanted}/temp.html")
 
 @app.route("/about")
 def about():
