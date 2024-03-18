57e54135cd70b3f6901b59286e3e240a6b95d775
Yamila Timmer <y.timmer.2@st.hanze.nl>
2024-03-18 21:43:45 +0100

    Added class for LePro-tool, made __init__, __str__ and run method.

---
+++ used_functions/classes/lepro_class.py
@@ -0,0 +1,32 @@
+"""
+MODULE DOCSTRING
+"""
+import subprocess
+class LePro:
+    """
+    class docstring
+    """
+    def __init__(self, pdb_file_name, **kwargs):
+        self.name_file = kwargs["name_file"]
+        self.pdb_file_name = pdb_file_name
+
+    def run(self):
+        """
+        method docstring
+        """
+        pdb_path = f"static/history/{self.name_file}/{self.pdb_file_name}"
+        #pdb_path = f"static/history/{self.name_file}/{self.pdb_file_name}"
+        lepro_path = subprocess.run(["find", "-name", "lepro_linux_x86"], check=True,
+                                    capture_output=True, text=True)
+        lepro_path = lepro_path.stdout.strip()
+        print(lepro_path, pdb_path)
+        print(subprocess.run([lepro_path, pdb_path], check=True, capture_output=True).stdout)
+
+
+    def __str__(self):
+        return f'File received:{self.pdb_file_name}, located in session: {self.name_file}.'
+
+if __name__ == "__main__":
+    lepro_instance = LePro("2BSM.pdb", name_file="webtool_test")
+    print(lepro_instance)
+    lepro_instance.run()
