2d2933399cf463d13ca70289143215b82732fce0
Ramon Reilman <r.reilman#st.hanze.nl>
2024-03-19 13:55:46 +0100

    changed line length for pylint

--- used_functions/classes/plip_tool.py
+++ used_functions/classes/plip_tool.py
@@ -1,21 +1,23 @@
 import subprocess
-from time import sleep
-import os
 class Plip():
     def __init__(self, path_to_pro, project_name):
         self.path_to_pro = path_to_pro
         self.project_name = project_name
-        
+
 
     def run(self):
-        
-        plip_path = subprocess.run(["find", "-name", "plipcmd.py"], check=True, capture_output=True, text=True)
+
+        plip_path = subprocess.run(["find", "-name", "plipcmd.py"],
+                                   check=True, capture_output=True, text=True)
         plip_path = plip_path.stdout.strip()
         path = f"static/history/{self.project_name}/pro.pdb"
-        subprocess.run(["python3",plip_path, "-f", path, "-p", "--peptides", "1","2", "3", "-o", f"static/history/{self.project_name}"], check=True)
+        print(path, plip_path)
+        subprocess.run(["python3", plip_path, "-f", path, "-p", "--peptides",
+                        "1","2", "3", "-o", f"static/history/{self.project_name}"], check=True)
 
     def __str__(self):
-        return f"Creates images using the {self.path_to_pro} file, and saves these images in static/history/{self.project_name}"
-    
+        return f"""Creates images using the {self.path_to_pro} file,
+                and saves these images in static/history/{self.project_name}"""
+
 plip = Plip("static/history/4zel.pdb/pro.pdb","4zel.pdb")
 plip.run()
