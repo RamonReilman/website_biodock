5425c1deef73024406458c9022770117954bbb92
Yamila Timmer <y.timmer.2@st.hanze.nl>
2024-03-20 20:02:51 +0100

    Changes did not save before, retry with changes for __str__ en __init__ in LePro class

--- used_functions/classes/lepro_class.py
+++ used_functions/classes/lepro_class.py
@@ -9,6 +9,8 @@ class LePro:
         self.pdb_save_path = pdb_save_path
         self.lepro_path = subprocess.run(["find", "-name", "lepro_linux_x86"], check=True,
                                     capture_output=True, text=True).stdout.strip()
+        self.new_save_path_dock = os.path.join("static/history/", self.name_file, "dock.in")
+        self.new_save_path_pro = os.path.join("static/history/", self.name_file, "pro.pdb")
 
     def run(self):
         """
@@ -20,15 +22,15 @@ class LePro:
         """
         method docstring
         """
-        new_save_path_dock = os.path.join("static/history/", self.name_file, "dock.in")
-        new_save_path_pro = os.path.join("static/history/", self.name_file, "pro.pdb")
-        subprocess.run(["mv", "dock.in", new_save_path_dock], check=True)
-        subprocess.run(["mv", "pro.pdb", new_save_path_pro], check=True)
-        print("New save:", new_save_path_dock)
+        subprocess.run(["mv", "dock.in", self.new_save_path_dock], check=True)
+        subprocess.run(["mv", "pro.pdb", self.new_save_path_pro], check=True)
 
     def __str__(self):
-        return f'LePro installation detected: {self.lepro_path}\n PDB file received \
-            by LePro:{self.pdb_save_path}, located in session: {self.name_file}.'
+
+        return (f'LePro installation detected: {self.lepro_path}\n'
+                f'PDB file received by LePro:{self.pdb_save_path}, located in session: {self.name_file}.\n'
+                f'Generated dock.in location: {self.new_save_path_dock}\n'
+                f'Generated pro.pdb location: {self.new_save_path_pro}\n')
 
 if __name__ == "__main__":
     lepro_instance = LePro("2BSM.pdb", name_file="webtool_test")
