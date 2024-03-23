404859c2c77aaa83c46e4a5271671027125eb9d1
Yamila Timmer <y.timmer.2@st.hanze.nl>
2024-03-23 18:06:37 +0100

    Fixed PEP8 errors in merge_pdb_dok.py

--- used_functions/merge_pdb_dok.py
+++ used_functions/merge_pdb_dok.py
@@ -11,6 +11,8 @@ Usage:
     Import into run_app.py
 """
 import string
+
+
 def process_lig_file(path_to_lig):
     """
     Opens ligand file and readies it to be merged with pro.pdb
@@ -24,6 +26,7 @@ def process_lig_file(path_to_lig):
     string_with_ligand =""
     amount_ends = 0
     alphabet = list(string.ascii_lowercase)
+
     # Opens ligand file
     with open(path_to_lig, 'r', encoding='utf-8') as ligand_file:
         for line in ligand_file:
@@ -34,12 +37,13 @@ def process_lig_file(path_to_lig):
                 amount_ends += 1
 
             else:
-                # Gives every ligand a chain name
-                # So that plip can render these ligands
+                # Gives every ligand a chain name, so that plip can render these ligands
                 if not line.startswith("REMARK"):
                     line = line.replace("LIG     0      ", f"LIG {alphabet[amount_ends]}   0      ")
                     string_with_ligand += line
+
     return string_with_ligand, amount_ends
+
 
 def update_pro(path_to_pro, lig_string):
     """
@@ -51,20 +55,26 @@ def update_pro(path_to_pro, lig_string):
     return new_string: returns combined string of pro.pdb and .dok file
 
     """
-    
+
     # Opens and reads pro.pdb file
     with open(path_to_pro, "r", encoding="utf-8") as pro_file:
         string_pdb = pro_file.read()
-        
+
         # Merges the ligand and pdb file
         new_string = lig_string + string_pdb
-    return new_string   
+
+    return new_string
+
 
 def main(pdb_file, lig_file):
+    """
+    DOCSTRING
+    """
     lig_string, n_ligands= process_lig_file(lig_file)
     new_string = update_pro(pdb_file, lig_string)
-    
+
     # Writes merged pdb and ligand file to new pro.pdb file.
     with open(pdb_file, 'w', encoding='utf-8') as file:
         file.write(new_string)
+
     return n_ligands
