eb124ff2a11f0f6eaf7141ea6cc57ebd6ab082ba
Yamila Timmer <y.timmer.2@st.hanze.nl>
2024-03-31 12:18:03 +0200

    Added docstrings and made sure test_functions.py is PEP8

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,16 +1,35 @@
 """
-MODULE DOCSTRING
+PyTest script for functions_used.py
+    - Authors: Ramon Reilman, Stijn Vermeulen, Yamila Timmer
+
+test_functions.py tests the following functions from functions_used.py:
+    - clear_me()
+    - save_settings()
+    - load_settings()
+    - settings_dok_file()
+    - mol2_to_ligands()
+
+Usage:
+    Run pytest to test the used functions, using this script.
+
+Commandline usage: 
+    - pytest
+
+
 """
 import os
 import json
-from used_functions.functions_used import clear_me, save_settings, load_settings, settings_dok_file, mol2_to_ligands, parse_config
+from used_functions.functions_used import clear_me, save_settings, load_settings, \
+    settings_dok_file, mol2_to_ligands
 
 
 def test_clear_me(tmpdir):
     """
     Tests clear_me() function.
+
+    :param tmpdir: creates a temporary dir to 'recreate' the original path
     """
-  
+
     # create a temporary file within the directory
     tmp_path = tmpdir.join('file.pdb')
     with open(tmp_path, 'w', encoding='utf-8') as tmp_file:
@@ -32,6 +51,8 @@ def test_clear_me(tmpdir):
 def test_save_settings(tmpdir):
     """
     Tests save_settings() function.
+
+    :param tmpdir: creates a temporary dir to 'recreate' the original path
     """
 
     # initiates needed variables
@@ -57,6 +78,8 @@ def test_save_settings(tmpdir):
 def test_load_settings(tmpdir):
     """
     Tests load_settings function.
+    
+    :param tmpdir: creates a temporary dir to 'recreate' the original path
     """
 
     json_object = {"a": 1, "b": 2, "c": 3}
@@ -76,7 +99,8 @@ def test_settings_dok_file():
     dock_slider = '18'
 
     # calls settings_dok_file function in which the dock.in file gets changed based on attributes
-    new_save_path_dock = settings_dok_file(new_save_path_dock='tests/dock.in', rmsd_slider=rmsd_slider, dock_slider=dock_slider)
+    new_save_path_dock = settings_dok_file(new_save_path_dock='tests/dock.in', \
+                                           rmsd_slider=rmsd_slider, dock_slider=dock_slider)
 
     # opens dock.in and asserts if lines 4 and 12 got changed to the given values
     with open(new_save_path_dock, 'r', encoding="utf-8") as dock_file_read:
@@ -84,18 +108,13 @@ def test_settings_dok_file():
         assert lines[4].strip() == rmsd_slider
         assert lines[12].strip() == dock_slider
 
-
     # reverts changes in dock.in file, so it's ready for future testing
     with open(new_save_path_dock, 'w', encoding="utf-8") as dock_file_read:
-
         for i, line in enumerate(lines):
-
             if i == 4:
                 dock_file_read.write('1.0' + '\n')
-
             elif i == 12:
                 dock_file_read.write('20' + '\n')
-
             else:
                 dock_file_read.write(line)
 
@@ -115,11 +134,3 @@ def test_mol2_to_ligands():
 
         # checks if the content of ligands_file is equal to the expected content (name of mol2 file)
         assert content == expected_content
-
-
-def test_parse_config():
-
-    parse_config()
-    return
-
-    
